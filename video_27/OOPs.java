c903f7631ab259123b4e1ed88e05415cc1bf05e2
Ravi kumar <160642860+raviraj2833@users.noreply.github.com>
2024-04-16 20:52:14 +0530

    Add files via upload

---
+++ video_27/OOPs.java
@@ -0,0 +1,21 @@
+
+public class OOPs {
+    public static void main(String args[]){
+        Horse h=new Horse();
+      
+        
+
+    }
+    
+}
+class Animal{
+    Animal(){
+        System.out.println("animal constructor is called");
+    }
+}
+class Horse extends Animal{
+    Horse(){
+        super();
+        System.out.println("horse constructor is called");
+    }
+}
